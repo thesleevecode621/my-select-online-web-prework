e03881cf9ae9dedd576b83198305073152d48eb9
boruch d <dubywider@gmail.com>
2019-04-08 16:29:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
-end
+ # your code here!	 # your code here!
+ if collection.count == 0
+ 	return nil
+ else
+		counter = 0
+		tmp_array = []
+		while counter < collection.count
+			if collection[counter] % 2 == 0
+				yield collection[counter]
+				tmp_array << collection[counter]
+				counter += 1
+			else
+				puts collection[counter]
+				counter += 1
+			end
+		end
+		return tmp_array
+ 	end
+end	
